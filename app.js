3975489ad09276e22f6cbbef30a153e8f0f816cc
PogodaIgor <igor.pogoda2010@wp.pl>
2025-10-23 22:42:12 +0200

    a

---
+++ app.js
@@ -0,0 +1,35 @@
+function Zmien_kolor() {
+  document.getElementById("czerwony").style.color = "red";
+  document.getElementById("imie").style.color = "red";
+}
+
+function Zmien_kolor_zielony() {
+  document.getElementById("imie").style.color = "green";
+  document.getElementById("zielony").style.color = "green";
+}
+
+function Zmien_tekst_zielony() {
+  document.getElementById("zielony").innerHTML = "Zielony";
+}
+
+function Wroc_tekst_czerwony() {
+  document.getElementById("czerwony").style.color = "black";
+}
+
+function Wroc_tekst_zielony() {
+  document.getElementById("zielony").innerHTML = "Zmień kolor";
+  document.getElementById("zielony").style.color = "black";
+}
+function Zmien_tekst_czerwony() {
+  document.getElementById("czerwony").innerHTML = "Czerwony";
+}
+function Wroc_tekst_czerwony() {
+  document.getElementById("czerwony").innerHTML = "Zmień kolor";
+  document.getElementById("czerwony").style.color = "black";
+}
+function Wroc_na_czarny() {
+  document.getElementById("czarny").innerHTML = "Cofnij";
+}
+function Zmien_na_czarny() {
+  document.getElementById("imie").style.color = "black";
+}
